c579c841a82bb99e7035a504724f0b5eb06fb6ab
seong954t <seomtae@naver.com>
2018-07-06 23:09:43 +0900

    add test script

---
+++ test_script.js
@@ -0,0 +1,23 @@
+function getRandomNumTest(){
+
+}
+
+function checkDuplicatedNumTest(lotto_nums, ball_num){
+    
+}
+
+function getLottoNumsTest(){
+    
+}
+
+function getWinningNumbersAndBonusNumberTest(){
+    
+}
+
+function getDuplicatedNumTest(lotto_nums){
+    
+}
+
+function getWinningResultTest(lotto_nums){
+    
+}
